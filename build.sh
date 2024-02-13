c0f248cc36aeb0e3f8dfc4459d9f85dfa7dd9f8f
Digno JR Teogalbo <dignojrteogalbo@gmail.com>
2024-02-12 19:24:36 -0700

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 python -m venv .venv
 
 .venv/bin/pip3 install jupyter ipython_genutils ipyvolume
+
+if [ "$ACTIONS_ENVIRONMENT" = true ]; then
+    echo "Running in Github action. Will not start JupyterLab."
+    exit 0
+fi
 
 .venv/bin/jupyter lab --notebook-dir=GoVizzy
