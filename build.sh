6a5261ee38d010758caff5cce52f76446be92119
dignojrteogalbo <49927429+dignojrteogalbo@users.noreply.github.com>
2024-02-12 21:48:48 -0700

    [#59] Created tests module (#73)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python -m venv .venv
 
-.venv/bin/pip3 install jupyter ipython_genutils ipyvolume
+.venv/bin/pip3 install jupyter ipython_genutils ipyvolume pytest
 
 if [ "$ACTIONS_ENVIRONMENT" = true ]; then
     echo "Running in Github action. Will not start JupyterLab."
