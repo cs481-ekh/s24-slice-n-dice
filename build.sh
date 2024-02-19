81973506d2db40a7ff1fac75bd4142c2d0c7b8b5
BraydenThompson <braydenthompson@u.boisestate.edu>
2024-02-18 19:18:42 -0700

    Install local user package

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-python -m venv .venv
+python -m venv --system-site-packages .venv
 
 .venv/bin/pip3 install jupyter ipython_genutils ipyvolume pytest ase
+.venv/bin/pip3 install --user ./GoVizzy/cube
 
 if [ "$ACTIONS_ENVIRONMENT" = true ]; then
     echo "Running in Github action. Will not start JupyterLab."
