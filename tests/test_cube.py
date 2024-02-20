abd8e03dec0ae14796a8d877af452c5300ee1efa
Brayden Thompson <braydenthompson@u.boisestate.edu>
2024-02-19 21:27:16 -0700

    [#68] Added user facing error messages (#88)

--- tests/test_cube.py
+++ tests/test_cube.py
@@ -1,6 +1,45 @@
 import cube_viskit as cv
+import pytest
 
 def testCubeImport():
     new_cube = cv.Cube()
-    new_cube.load_cube("GoVizzy/data/rhopol.cube")
-    assert new_cube.fname == "GoVizzy/data/rhopol.cube" 
+    new_cube.load_cube("tests/data/rhopol.cube")
+    assert new_cube.fname == "tests/data/rhopol.cube"
+
+def testNoFileErr():
+    new_cube = cv.Cube()
+    with pytest.raises(SystemExit) as exc_info:
+        new_cube.load_cube("")
+    
+    assert exc_info.value.code == -1
+    
+def testHeaderErr():
+    new_cube = cv.Cube()
+    with pytest.raises(SystemExit) as exc_info:
+        new_cube.load_cube("tests/data/rhopol_err_header.cube")
+    
+    assert exc_info.value.code == -2
+
+
+def testAtomsErr():
+    new_cube = cv.Cube()
+    with pytest.raises(SystemExit) as exc_info:
+        new_cube.load_cube("tests/data/rhopol_err_atoms.cube")
+    
+    assert exc_info.value.code == -3
+    
+def testDimensionsErr():
+    new_cube = cv.Cube()
+    with pytest.raises(SystemExit) as exc_info:
+        new_cube.load_cube("tests/data/rhopol_err_dimensions.cube")
+    
+    assert exc_info.value.code == -4
+    
+def testFieldDataErr():
+    new_cube = cv.Cube()
+    with pytest.raises(SystemExit) as exc_info:
+        new_cube.load_cube("tests/data/rhopol_err_data.cube")
+    
+    assert exc_info.value.code == -4
+    
+
