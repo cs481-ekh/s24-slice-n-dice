d47630b9925b7d697af33a7b06041a4499691e2c
Brayden Thompson <braydenthompson@u.boisestate.edu>
2024-02-19 18:52:53 -0700

    [#82] Added OS checking to scripts (#84)

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# Determnine OS type for venv directories
+binDir="bin" # Default for linux / macos
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    binDir="Scripts"
+fi
+
 if [ ! -d .venv ]; then
     echo "No .venv found! Did you forget to run build.sh?"
     exit 1
 fi
 
-.venv/bin/python -m pytest
+.venv/$binDir/python -m pytest
