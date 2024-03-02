b6b7bdd42e3daec9cb7b0b37a403c7a1ae7ca94a
BraydenThompson <braydenthompson@u.boisestate.edu>
2024-03-02 14:55:22 -0700

    Added slider

--- GoVizzy/widgets.py
+++ GoVizzy/widgets.py
@@ -1,15 +1,27 @@
 '''This File is for widgets used by GoVizzy
     Input form: widget for uploading a .cube file
+    Options: Widget containing options for customization of output.
     Documentation for widget library: https://ipywidgets.readthedocs.io/en/latest/examples/Widget%20List.html#file-upload
 '''
 from IPython.display import display
-from ipywidgets import Layout, Button, Box, Textarea, Label
+from ipywidgets import Layout, Button, Box, Textarea, Label, FloatSlider
 
+# Input form
 # Layout for Input form
 form_item_layout = Layout(
     display='flex',
     flex_flow='row',
     justify_content='space-between'
+)
+
+test_slider = FloatSlider(
+    value=0,
+    min=0,
+    max=10,
+    step=0.1,
+    description='Example slider!',
+    readout=True,
+    readout_format='.1f'
 )
 
 # Input form items
@@ -18,6 +30,7 @@ form_items = [
     Box([Label(value='Path to .cube file'), 
          Textarea()], layout=form_item_layout),
     Button(description='Submit', layout=Layout(flex='1 1 0%', width='auto')),
+    test_slider,
     
 ]
 
@@ -29,4 +42,4 @@ form = Box(form_items, layout=Layout(
     align_items='stretch',
     width='50%'
 ))
-form
+form, test_slider
