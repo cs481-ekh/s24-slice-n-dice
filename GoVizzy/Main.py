681da627db904a62412aaaff87c509f0d229c6d1
ryliewalsh <62161188+ryliewalsh@users.noreply.github.com>
2024-03-12 14:26:47 -0600

    PR for #114  (#139)
    * Working UI menu and Display
    
    * New Cube button is working
    
    * Exit button clears output but message is not appearing
    
    * minor updates
    
    * Exit message updated
    
    * Exit message added

--- GoVizzy/Main.py
+++ GoVizzy/Main.py
@@ -3,9 +3,12 @@ import ipywidgets as widgets
 import os
 import cube_viskit as cv
 import matplotlib.pyplot as plt
-from ipywidgets import AppLayout, Button, HBox, Layout, HBox, Output, Dropdown
-from IPython.display import display
+from ipywidgets import AppLayout, Button, HBox, Layout, HBox, Output, Dropdown, Textarea
+from IPython.display import display, clear_output
 from UI import DisplayUI
+import signal
+import sys
+
 
 
 # Define a text input widget
@@ -40,19 +43,37 @@ def handle_submit_button_clicked(b):
                 file_input.layout.visibility = 'hidden'  # Hide the file input widget
                 submit_button.layout.visibility = 'hidden'  # Hide the submit button widget
 
+
+            # Handlers for dropdown change and button clicks
             DisplayUI.dropdown.observe(
                     lambda change: DisplayUI.handle_dropdown_change(change, new_cube),
                     names='value'
                 )
+            DisplayUI.newCube_button.on_click(handle_newCube_click)
+            
                 
         else:
             print(f"Source file not found: {fileName}")
     else:
         print("Enter a valid .cube file name")
 
+
+
+def handle_exit_click(b):
+    file_input.layout.visibility = 'hidden'  # Hide the file input widget
+    submit_button.layout.visibility = 'hidden'  # Hide the submit button widget
+    DisplayUI.clear_all_outputs()
+    
+    
+def handle_newCube_click(b):
+    DisplayUI.show_menu()
+    file_input.layout.visibility = 'visible'
+    submit_button.layout.visibility = 'visible'
+    
 def main():
     global file_input, submit_button
-    DisplayUI.hide_ui()
+    
+    DisplayUI.show_menu()
     # Define a text input widget
     file_input = widgets.Text(description='File Name:')
 
@@ -63,7 +84,11 @@ def main():
     submit_button.on_click(handle_submit_button_clicked)
 
     # Display the widgets
-    display(HBox([file_input, submit_button]))
+    file_input_button = HBox((file_input, submit_button), layout=Layout(justify_content='center'))
+
+    DisplayUI.exit_button.on_click(handle_exit_click)
+    
+    display(file_input_button)
 
 # Call the main function
 if __name__ == "__main__":
